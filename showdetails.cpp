d8bfacf9be5e1b5db52df9b5fda6f4a9a69814e5
aniruddh <aniruddh.sxn22@gmail.com>
2014-07-26 21:42:43 +0530

    Update showdetails.cpp

--- showdetails.cpp
+++ showdetails.cpp
@@ -1,2 +1,13 @@
-int showdetails()
+int showdetails(char a[])
 {
+  int f,i;
+  for(i=1;i<100;i++)
+    {
+      st[]=game();
+      if(!strcmp(a,st))
+      f=1;
+      else 
+      f=0;
+    }
+return f;
+}
