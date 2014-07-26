a10fd93f70075c2e00f89b14e40d1221760986a0
aniruddh <aniruddh.sxn22@gmail.com>
2014-07-26 20:59:12 +0530

    Update function1.cpp

--- function1.cpp
+++ function1.cpp
@@ -2,19 +2,19 @@ void choice()
 {
 int ch,l;
 char name[500];
-cout<<"welcome \n 1.To find book /n 2.to buy book /n 3.TO rent book 4. to check the all the books/n enter your choice";
+cout<<"welcome \n 1.To find game /n 2.to buy game /n 3.TO rent game 4. to check the all the games/n enter your choice";
 cin>>ch;
 switch(ch)
 {
 case 1:
   {
-  cout<<"enter the name of book you want to serch";
+  cout<<"enter the name of game you want to serch";
   gets(name);
   l=database(name);  
   if(l==1)
-  cout<<"book is in the database";
+  cout<<"game is in the database";
   else if(l==2)
-  cout<<"book is out of stock";
+  cout<<"game is out of stock";
   else
   cout<<"book not found";
-  
+  }
