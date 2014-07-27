4128b7f5702feede4913fea7ab7f68c2b0a3777a
aniruddh <aniruddh.sxn22@gmail.com>
2014-07-27 12:29:52 +0530

    Create showdetails.cpp

---
+++ showdetails.cpp
@@ -0,0 +1,17 @@
+void showdetails(char a[])
+{int i;
+char a[100];
+for(i=0;i<10;i++)
+{
+str[]=game(a);
+if(!strcmp(str,name))
+{
+cout<<"name of the game is \t""
+puts(name);
+cout<<"\n type of game\t"<<type;
+cout<<"\n price of game \t"<<price;
+cout<<"\n available in stock\t"<<stock;
+}
+else 
+continue;
+}
